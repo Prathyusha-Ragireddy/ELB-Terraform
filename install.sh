107f4ceb93994955155c21083cddc19cd3f95890
prathyusha <ragireddy.prathyusha@gmail.com>
2019-06-25 11:48:56 -0400

    Updated with apache

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-yum -y install httpd
-systemctl start httpd.service
-echo "<html><h1>Hello from mlabouardy ^^</h2></html>" > /var/www/html/index.html
+yum install -y httpd
+service httpd start
+chkonfig httpd on
+echo "<html><h1>Hello World!!</h2></html>" > /var/www/html/index.html
