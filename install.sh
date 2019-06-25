7d20d4c02f74d1d51c2f51ff8f40cb2e82fc8d35
prathyusha <ragireddy.prathyusha@gmail.com>
2019-06-25 13:41:18 -0400

    Added server Ipaddress

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 yum install -y httpd
 service httpd start
 chkonfig httpd on
-echo "<html><h1>Hello World!!</h2></html>" > /var/www/html/index.html
+serverip=`/sbin/ifconfig eth0 | grep "inet" | awk '{print $2}' | awk 'NR==1' | cut -d':' -f2`
+echo "<html><h1>Hello from mlabouardy ^^</h1> <h2> Server IPAddress serving you is $serverip </h2> <</html>" > /var/www/html/index.html
+
