3b88df01076231ce639b8f2edf2d6a938e3d07a8
prathyusha <ragireddy.prathyusha@gmail.com>
2019-06-25 13:41:50 -0400

    Updated the naming

--- main.tf
+++ main.tf
@@ -1,58 +1,62 @@
-# Provider - specifying the cloud you want to use
-provider "aws"{
-region = "${var.aws_region}"
+# Provider - Name of the cloud
+provider "aws" {
+  region = "${var.aws_region}"
 }
 
 # Define VPC
-resource "aws_vpc" "main"{
-cidr_block       = "10.0.0.0/16"
-enable_dns_hostnames = true
-tags = {
-    Name = "ELB-VPC"
+resource "aws_vpc" "main" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC"
   }
 }
 
-# Define Public DNS
+# Define  public subnet
 resource "aws_subnet" "public-subnet" {
- vpc_id = "${aws_vpc.main.id}"
- cidr_block = "${var.public_subnet_cidr}"
- availability_zone = "us-east-1a"
- tags = {
- Name = "ELB-VPC Public Subnet"
- }
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "${var.public_subnet_az}"
+  map_public_ip_on_launch= true
+
+  tags = {
+    Name = "VPC Public Subnet"
+  }
 }
 
+# Define the private subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "${var.private_subnet_cidr}"
+  availability_zone = "${var.private_subnet_az}"
 
-# Define Public DNS
-resource "aws_subnet" "Private-subnet" {
- vpc_id = "${aws_vpc.main.id}"
- cidr_block = "${var.private_subnet_cidr}"
- availability_zone = "us-east-1a"
- tags = {
- Name = "ELB-VPC Private Subnet"
- }
+  tags = {
+    Name = "VPC Private Subnet"
+  }
 }
 
-# Define Internet Gateway
+# Define the internet gateway
 resource "aws_internet_gateway" "IGW" {
-vpc_id = "${aws_vpc.main.id}"
-tags = {
-Name = "ELB-VPC IGW"
-}
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "VPC IGW"
+  }
 }
 
-# Define Route Table
+# Define the route table
 resource "aws_route_table" "public-RT" {
-vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.main.id}"
 
-route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.IGW.id}"
-}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.IGW.id}"
+  }
 
-tags = {
-Name = "ELB-VPC public Route Table"
-}
+  tags = {
+    Name = "VPC Public Subnet RT"
+  }
 }
 
 # Assign the route table to the public Subnet
@@ -60,21 +64,15 @@ resource "aws_route_table_association" "public-RT" {
   subnet_id = "${aws_subnet.public-subnet.id}"
   route_table_id = "${aws_route_table.public-RT.id}"
 }
+
 # Define the security group for public subnet
 resource "aws_security_group" "sgweb" {
-  name = "vpc_test_web"
+  name = "vpc_web"
   description = "Allow incoming HTTP connections & SSH access"
 
   ingress {
     from_port = 80
     to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -93,21 +91,28 @@ resource "aws_security_group" "sgweb" {
     cidr_blocks =  ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   vpc_id="${aws_vpc.main.id}"
 
   tags = {
-    Name = "Web Server SG"
+    Name = "ELB-SG-terraform"
   }
 }
+
+
 # Define SSH key pair for our instances
 resource "aws_key_pair" "default" {
-  key_name = "vpckeypair"
+  key_name = "terraform"
   public_key = "${file("${var.key_path}")}"
 }
 
-
-
-# Creating Launch Configuration
+## Creating Launch Configuration
 resource "aws_launch_configuration" "test" {
   image_id = "${var.ami}"
   instance_type = "${var.instance_type}"
@@ -116,7 +121,7 @@ resource "aws_launch_configuration" "test" {
   user_data = "${file("install.sh")}"
 }
 
-# Creating AutoScaling Group
+## Creating AutoScaling Group
 resource "aws_autoscaling_group" "test" {
   launch_configuration = "${aws_launch_configuration.test.id}"
   vpc_zone_identifier       = ["${aws_subnet.public-subnet.id}"]
@@ -124,8 +129,29 @@ resource "aws_autoscaling_group" "test" {
   max_size = 10
   load_balancers = ["${aws_elb.app.name}"]
   health_check_type = "ELB"
-  associate_public_ip_address = true
-  
+
+}
+
+resource "aws_autoscaling_policy" "staging_memory" {
+  name = "memory-reservation"
+
+  autoscaling_group_name = "${aws_autoscaling_group.test.id}"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    customized_metric_specification {
+      metric_dimension {
+        name  = "ClusterName"
+        value = "staging"
+      }
+
+      metric_name = "MemoryReservation"
+      namespace   = "AWS/ECS"
+      statistic   = "Maximum"
+    }
+
+    target_value = "75"
+  }
 }
 
 resource "aws_elb" "app" {
@@ -138,7 +164,7 @@ resource "aws_elb" "app" {
   listener {
     lb_port = 80
     lb_protocol = "http"
-    instance_port = "8080"
+    instance_port = "80"
     instance_protocol = "http"
   }
   health_check {
@@ -146,20 +172,20 @@ resource "aws_elb" "app" {
     unhealthy_threshold = 2
     timeout = 3
     interval = 30
-    target = "HTTP:8080/"
+    target = "HTTP:80/"
   }
   cross_zone_load_balancing   = true
   idle_timeout                = 960  # set it higher than the conn. timeout of the backend servers
   connection_draining         = true
   connection_draining_timeout = 300
   tags = {
-    Name = "test-elb-app"
+    Name = "ELB-Classic"
     Type = "elb"
   }
 }
 
 resource "aws_security_group" "elb" {
-    name = "test-elb"
+    name = "ElB-SG"
     ingress {
         from_port   = 80
         to_port     = 80
@@ -180,6 +206,6 @@ resource "aws_security_group" "elb" {
     }
     vpc_id = "${aws_vpc.main.id}"
     tags = {
-        Name        = "test-elb-security-group"
+        Name        = "ELB-security-group"
     }
 }
